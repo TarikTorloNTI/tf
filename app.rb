1a0ebc02a98e631a8eced8e8c20ee4208985e6cd
TarikTorloNTI <tarik.torlo@elev.ga.ntig.se>
2024-01-31 09:21:48 +0100

    Kodning v.5
    Påbörjade kod för app.rb

--- app.rb
+++ app.rb
@@ -6,5 +6,19 @@ require 'bcrypt'
 enable :sessions
 
 get('/') do
-    slim(:hemsida)
+    slim(:start)
 end
+
+
+get ('/gymlog') do
+    db = SQLite3::Database.new("db/user.db")
+    result = db.execute('SELECT * FROM gymlog')
+    db.results_as_hash = true
+    p result
+    p "Hello"
+  end
+  
+
+# get('/gymlog/new') do
+#     slim(:gymlog/new)
+# end
