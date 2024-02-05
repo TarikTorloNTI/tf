fe44d6ff47bc02aed11baa7aaf63927c29c4ddfe
TarikTorloNTI <tarik.torlo@elev.ga.ntig.se>
2024-02-05 14:41:02 +0100

    5/2 - 2024 | v.5
    Idag har jag fortsatt att koda och göra layout för CRUD till min hemsida. Jag har även följt MVC för min gymsida där man ska kunna ta bort en logg och lägga till en logg.

--- app.rb
+++ app.rb
@@ -2,23 +2,44 @@ require 'sinatra'
 require 'slim'
 require 'sqlite3'
 require 'bcrypt'
+require 'sinatra/reloader'
 
 enable :sessions
+user-id = 1
+user-id = session[:key1].to_i
+
 
 get('/') do
     slim(:start)
 end
 
 
-get ('/gymlog') do
+get('/gymlog') do
     db = SQLite3::Database.new("db/user.db")
-    result = db.execute('SELECT * FROM gymlog')
     db.results_as_hash = true
-    p result
-    p "Hello"
-  end
-  
+    @result = db.execute("SELECT * FROM gymlog")
 
-# get('/gymlog/new') do
-#     slim(:gymlog/new)
-# end
+    slim(:"gymlog/index")
+end
+
+
+post('/gymlog/:id/delete') do
+  id = params[:id].to_i
+  db = SQLite3::Database.new("db/user.db")
+  db.execute("DELETE FROM gymlog WHERE id = ?",id)
+  redirect('/gymlog')
+end
+
+
+get('/gymlog/new') do
+  slim(:"gymlog/new")
+end
+
+
+post('/gymlog/new') do
+  dag = params[:dag]
+  exercise = params[:exercise].to_i
+  db = SQLite3::Database.new("db/user.db")
+  db.execute("INSERT INTO gymlog (dag, exercise) VALUES (?,?)", dag, exercise)
+  redirect('/gymlog')
+end
